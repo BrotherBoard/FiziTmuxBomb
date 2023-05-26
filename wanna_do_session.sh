e452e91e66e8d6ec4faaf09f36d44d0b16b553fd
BrotherBoard <82703813+BrotherBoard@users.noreply.github.com>
2023-05-26 06:12:38 +0300

    gay is no more

--- wanna_do_session.sh
+++ wanna_do_session.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # inside fizi folder in ubuntu
-gen_ss = 1
+gen_ss=1
 while [[ $gen_ss == 1 ]]
 do
 read -e -p "do you wanna genereate STRING_SESSION? (Y/n)" lmao
@@ -8,18 +8,18 @@ if [[ $lmao == "Y" ]]
 then
 apt-get install curl
 bash session_gen.sh
-gen_ss = 0
+gen_ss=0
 elif [[ $lmao == "y" ]]
 then
 apt-get install curl
 bash session_gen.sh
-gen_ss = 0
+gen_ss=0
 elif [[ $lmao == "n" ]]
 then
-gen_ss = 0
+gen_ss=0
 elif [[ $lmao == "N" ]]
 then
-gen_ss = 0
+gen_ss=0
 else
 echo -e "unknown option $lmao, supported inputs are [Y, y, N, n], try again"
 sleep 2
