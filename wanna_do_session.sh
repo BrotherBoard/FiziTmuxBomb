b0fb87b32e3aa1389d978503be0a3cc160cba731
BrotherBoard <82703813+BrotherBoard@users.noreply.github.com>
2023-05-26 06:07:07 +0300

    no more gay

--- wanna_do_session.sh
+++ wanna_do_session.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # inside fizi folder in ubuntu
+gen_ss = 1
 while [[ $gen_ss == 1 ]]
 do
 read -e -p "do you wanna genereate STRING_SESSION? (Y/n)" lmao
@@ -19,5 +20,8 @@ gen_ss = 0
 elif [[ $lmao == "N" ]]
 then
 gen_ss = 0
+else
+echo -e "unknown option $lmao, supported inputs are [Y, y, N, n], try again"
+sleep 2
 fi
 done
